ea8288d824a8c5eb3781be47c901ef65cb7e53ea
focusjuice <122142408+focusjuice@users.noreply.github.com>
2023-05-19 16:40:55 +0200

    added variables to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const gameBoard = document.querySelector("#gameboard")
+const playerDisplay = document.querySelector("#player")
+const infoDisplay = document.querySelector("#info-display")
+const width = 8
+
+const startPieces = [
+
+]
