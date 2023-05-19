e60e6b065f09e1a2f23e4506d7bf6e21b40cba96
focusjuice <122142408+focusjuice@users.noreply.github.com>
2023-05-19 17:04:54 +0200

    added colores to the gameboard in styles.css

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 const startPieces = [
-
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+function createBoard() {
+    startPieces.forEach((startPiece) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+    })
+}
