2dd32b5ea7074099a61f2acb7a156175d01530e0
focusjuice <122142408+focusjuice@users.noreply.github.com>
2023-05-19 22:07:35 +0200

    added squares and diffrent color sides

--- app.js
+++ app.js
@@ -19,8 +19,26 @@ function createBoard() {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
+        square.firstChild && square.firstChild.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         square.classList.add('beige')
+        // square.classList.add('beige')
+        const row = Math.floor((63 - i) / 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? 'beige' : 'brown')
+        } else {
+            square.classList.add(i % 2 === 0 ? 'brown' : 'beige')
+        }
+
+        if (i <= 15) {
+            square.firstChild.firstChild.classList.add('black')
+        }
+
+        if (i >= 48) {
+            square.firstChild.firstChild.classList.add('white')
+        }
+
+
         gameBoard.append(square)
     })
 }
